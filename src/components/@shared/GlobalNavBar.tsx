2a83e1b80b391e5fa85993944098e182639baf9a
leehj322 <gudwns8956@naver.com>
2024-08-02 21:11:42 +0900

    refactor(shared): GlobalNavBar 수정

--- src/components/@shared/GlobalNavBar.tsx
+++ src/components/@shared/GlobalNavBar.tsx
@@ -5,11 +5,11 @@ import userPfpImg from "../../assets/images/img_basic_user_pfp.png";
 import { useLocation, Link } from "react-router-dom";
 
 interface StyledProps {
-  isMain: boolean;
+  $isMain: boolean;
 }
 
 interface NavMenuProps {
-  isHere: boolean;
+  $isHere: boolean;
 }
 
 interface GlobalNavBarProps {
@@ -17,8 +17,16 @@ interface GlobalNavBarProps {
   isMain: boolean;
 }
 
-const StyledNavContainer = styled.nav`
+const StyledStickyNavContainer = styled.nav`
   background-color: ${({ theme }) => theme.colors.gray00};
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.headerUnderline};
+`;
+
+const StyledNavContainer = styled.div`
   display: flex;
   max-width: 1200px;
   height: 70px;
@@ -62,8 +70,8 @@ const StyledLogoImg = styled.img<StyledProps>`
 
 const StyledTypoImg = styled.img<StyledProps>`
   display: none;
-  width: ${({ isMain }) => (isMain ? "103px" : "81px")};
-  height: ${({ isMain }) => (isMain ? "51px" : "40px")};
+  width: ${({ $isMain }) => ($isMain ? "103px" : "81px")};
+  height: ${({ $isMain }) => ($isMain ? "51px" : "40px")};
 
   @media ${({ theme }) => theme.device.mobile} {
     display: block;
@@ -80,7 +88,7 @@ const StyledNavMenusArea = styled.div`
 
 const StyledNavMenuWrapper = styled.div<NavMenuProps>`
   height: 100%;
-  color: ${({ isHere, theme }) => (isHere ? theme.colors.brandBlue : theme.colors.gray600)};
+  color: ${({ $isHere, theme }) => ($isHere ? theme.colors.brandBlue : theme.colors.gray600)};
   font-size: 18px;
   font-weight: 700;
   padding: 0 15px;
@@ -100,10 +108,10 @@ const StyledUserPfpImg = styled.img`
 const StyledLoginBtn = styled.button<StyledProps>`
   background-color: ${({ theme }) => theme.colors.brandBlue};
   color: ${({ theme }) => theme.colors.gray100};
-  font-size: ${({ isMain }) => (isMain ? "18px" : "16px")};
+  font-size: ${({ $isMain }) => ($isMain ? "18px" : "16px")};
   font-weight: 600;
-  width: ${({ isMain }) => (isMain ? "128px" : "88px")};
-  height: ${({ isMain }) => (isMain ? "48px" : "42px")};
+  width: ${({ $isMain }) => ($isMain ? "128px" : "88px")};
+  height: ${({ $isMain }) => ($isMain ? "48px" : "42px")};
   border-radius: 8px;
 `;
 
@@ -111,29 +119,37 @@ function GlobalNavBar({ isMain, isLogin }: GlobalNavBarProps) {
   const { pathname: path } = useLocation();
 
   return (
-    <StyledNavContainer>
-      <StyledHeaderLeftArea>
-        <StyledLogoImg isMain={isMain} src={headerLogoImg} alt="로고이미지" />
-        <StyledTypoImg isMain={isMain} src={headerTypoImg} alt="로고이미지" />
-        {!isMain && (
-          <StyledNavMenusArea>
-            <StyledNavMenuWrapper isHere={path.includes("자유게시판링크")}>
-              자유게시판
-            </StyledNavMenuWrapper>
-            <StyledNavMenuWrapper isHere={path.includes("/items")}>중고마켓</StyledNavMenuWrapper>
-          </StyledNavMenusArea>
-        )}
-      </StyledHeaderLeftArea>
-      <div>
-        {isLogin ? (
-          <StyledUserPfpImg src={userPfpImg} alt="프로필이미지" />
-        ) : (
-          <Link to="/login">
-            <StyledLoginBtn isMain={isMain}>로그인</StyledLoginBtn>
+    <StyledStickyNavContainer>
+      <StyledNavContainer>
+        <StyledHeaderLeftArea>
+          <Link to="/">
+            <StyledLogoImg $isMain={isMain} src={headerLogoImg} alt="로고이미지" />
+            <StyledTypoImg $isMain={isMain} src={headerTypoImg} alt="로고이미지" />
           </Link>
-        )}
-      </div>
-    </StyledNavContainer>
+          {!isMain && (
+            <StyledNavMenusArea>
+              <StyledNavMenuWrapper $isHere={path.includes("자유게시판링크")}>
+                자유게시판
+              </StyledNavMenuWrapper>
+              <Link to="/items">
+                <StyledNavMenuWrapper $isHere={path.includes("/items")}>
+                  중고마켓
+                </StyledNavMenuWrapper>
+              </Link>
+            </StyledNavMenusArea>
+          )}
+        </StyledHeaderLeftArea>
+        <div>
+          {isLogin ? (
+            <StyledUserPfpImg src={userPfpImg} alt="프로필이미지" />
+          ) : (
+            <Link to="/login">
+              <StyledLoginBtn $isMain={isMain}>로그인</StyledLoginBtn>
+            </Link>
+          )}
+        </div>
+      </StyledNavContainer>
+    </StyledStickyNavContainer>
   );
 }
 
